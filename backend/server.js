378579f523e38e485e447980b398879759c8da20
Saad <46577833+saads312@users.noreply.github.com>
2025-03-28 10:05:32 +0400

    setup node js server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
 require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.json()); // Allows JSON requests
+
+// Sample API routes
+app.get('/', (req, res) => {
+    res.send('Backend is running...');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
