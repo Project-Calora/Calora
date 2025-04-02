a11802d01570ca89844ba89abb80588f7de90c13
Kevin Patel <90125092+pateruu@users.noreply.github.com>
2025-04-02 09:13:00 -0400

    removing footer
    * Created kevin branch to add changes.
    
    * Added custom footer, and removed the 'get started by...' text

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,4 @@
 import { Link } from "@heroui/link";
-import { Snippet } from "@heroui/snippet";
-import { Code } from "@heroui/code";
 import { button as buttonStyles } from "@heroui/theme";
 
 import { siteConfig } from "@/config/site";
@@ -44,15 +42,6 @@ export default function IndexPage() {
             <GithubIcon size={20} />
             GitHub
           </Link>
-        </div>
-
-        <div className="mt-8">
-          <Snippet hideCopyButton hideSymbol variant="bordered">
-            <span>
-              Get started by editing{" "}
-              <Code color="primary">pages/index.tsx</Code>
-            </span>
-          </Snippet>
         </div>
       </section>
     </DefaultLayout>
