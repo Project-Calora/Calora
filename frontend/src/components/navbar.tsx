d808c4fa8ffc1d7d3edb826ec5ccdeb6bd027300
Kevin Patel <kxvin.pxtel1@gmail.com>
2025-04-20 15:44:42 -0400

    Changed UI layout to one page style. Made other minor adjustments to main page

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -51,10 +51,10 @@ export const Navbar = () => {
           <Link
             className="flex justify-start items-center gap-1"
             color="foreground"
-            href="/"
+            href="#home"
           >
             <Logo />
-            <p className="font-bold text-inherit">ACME</p>
+            <p className="font-bold text-inherit">Calora</p>
           </Link>
         </NavbarBrand>
         <div className="hidden lg:flex gap-4 justify-start ml-2">
@@ -80,29 +80,28 @@ export const Navbar = () => {
         justify="end"
       >
         <NavbarItem className="hidden sm:flex gap-2">
-          <Link isExternal href={siteConfig.links.github} title="GitHub">
+          <Link href={siteConfig.links.github} title="GitHub">
             <GithubIcon className="text-default-500" />
           </Link>
           <ThemeSwitch />
         </NavbarItem>
         <NavbarItem className="hidden md:flex">
-            <Link
-                isExternal
-                className={buttonStyles({
-                color: "customGreen",
-                radius: "full",
-                variant: "shadow",
-                size: "sm",
-                })}
-                href={siteConfig.links.docs}
-            >
-                Log In
-            </Link>
+          <Link
+            className={buttonStyles({
+              color: "customGreen",
+              radius: "full",
+              variant: "shadow",
+              size: "sm",
+            })}
+            href={"#contact"} // Change to anchor if you have a contact section
+          >
+            Log In
+          </Link>
         </NavbarItem>
       </NavbarContent>
 
       <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-        <Link isExternal href={siteConfig.links.github}>
+        <Link href={siteConfig.links.github}>
           <GithubIcon className="text-default-500" />
         </Link>
         <ThemeSwitch />
@@ -113,16 +112,16 @@ export const Navbar = () => {
         {searchInput}
         <div className="mx-4 mt-2 flex flex-col gap-2">
           {siteConfig.navMenuItems.map((item, index) => (
-            <NavbarMenuItem key={`${item}-${index}`}>
+            <NavbarMenuItem key={`menu-${item.label}-${index}`}>
               <Link
                 color={
                   index === 2
                     ? "primary"
                     : index === siteConfig.navMenuItems.length - 1
-                      ? "danger"
-                      : "foreground"
+                    ? "danger"
+                    : "foreground"
                 }
-                href="#"
+                href={item.href}
                 size="lg"
               >
                 {item.label}
